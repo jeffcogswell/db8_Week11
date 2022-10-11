07a15bd2dde6f22e59983edeee3b05eddb3ee179
Jeff Cogswell <jeffcogs@gmail.com>
2022-10-11 12:11:55 -0400

    Init

---
+++ XkcdConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿// Main API Page: https://xkcd.com/json.html
+
+// https://xkcd.com/info.0.json
+
+Console.WriteLine("Which comic number would you like to see?");
+string entry = Console.ReadLine();
+
+// ====================
+// These four lines connect to an API server and get back an instance of our class
+HttpClient web = new HttpClient();
+web.BaseAddress = new Uri("https://xkcd.com/");
+var connection = await web.GetAsync($"{entry}/info.0.json");
+Comic com = await connection.Content.ReadAsAsync<Comic>();
+// ====================
+
+//string result = await connection.Content.ReadAsStringAsync();
+//Console.WriteLine(result);
+Console.WriteLine(com.alt);
+Console.WriteLine(com.img);
+
+class Comic
+{
+	public int month { get; set; }
+	public int num { get; set; }
+	public string link { get; set; }
+	public int year { get; set; }
+	public string news { get; set; }
+	public string safe_title { get; set; }
+	public string transcript { get; set; }
+	public string alt { get; set; }
+	public string img { get; set; }
+	public string title { get; set; }
+	public int day { get; set; }
+}
---
+++ XkcdMvc/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using XkcdMvc.Models;
+
+namespace XkcdMvc.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		async public Task<IActionResult> Index()
+		{
+			HttpClient web = new HttpClient();
+			web.BaseAddress = new Uri("https://xkcd.com/");
+			var connection = await web.GetAsync("info.0.json");
+			Comic com = await connection.Content.ReadAsAsync<Comic>();
+			return View(com);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
